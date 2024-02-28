990b015c23d673c8e490dc62f88924272f6b47e0
Ахмед Сулиман <115305520+AhmdSolimn777@users.noreply.github.com>
2024-02-28 21:47:36 +0200

    projext reset

--- app/page.tsx
+++ app/page.tsx
@@ -1,9 +1,11 @@
 import Image from "next/image";
+import { Button } from "@/components/ui/button"
 
 export default function Home() {
   return (
     <>
-      <h1 className="text-slate-200">Home Page</h1>
+      {/* <h1 className="text-slate-200">Home Page</h1> */}
+      <Button variant="outline">Button</Button>
     </>
   );
 }
