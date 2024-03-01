90779225ea3c71148298beed84cbb8126a02d843
Ахмед Сулиман <115305520+AhmdSolimn777@users.noreply.github.com>
2024-03-01 18:22:56 +0200

    complete home page

--- app/home/page.tsx
+++ app/home/page.tsx
@@ -1,10 +1,13 @@
+import MovieVideo from "../components/MovieVideo";
 import Navbar from "../components/Navbar";
+import RecentlyAdded from "../components/RecentlyAdded";
 
 export default function HomePage() {
   return (
-    <div className="">
-      <Navbar />
-      <h1>Hello authenticated user</h1>
+    <div className="p-5 lg:p-0">
+      <MovieVideo />
+      <h1 className="text-3xl font-bold">Recently added</h1>
+      <RecentlyAdded />
     </div>
   );
 }
